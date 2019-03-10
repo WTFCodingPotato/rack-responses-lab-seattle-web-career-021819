4c488277947fc8c8861e81ca0015efa92cbf2806
Joshua M Loran <joshua.loran@gmail.com>
2019-03-10 18:54:25 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+  
+  def call(env)
+    resp Rack::Response.new
+    if Time.new.hour >11
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+    resp.finish
+  end
+end
